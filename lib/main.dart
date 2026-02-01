e9aa2e17209fc9d6e159a9473516e41c0340f727
José Viloria <joseviloria@correo.unimet.edu.ve>
2026-01-31 23:51:41 -0400

    Primer commit, pantallas iniciales

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:microproyecto_si/GameScreen.dart';
+import 'package:microproyecto_si/Leaderboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const TitleScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class TitleScreen extends StatelessWidget {
+  const TitleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 20,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const GameScreen()),
+                  );
+                },
+                child: Text('Jugar', style: const TextStyle(fontSize: 50)),
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              height: 75,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Leaderboard()),
+                  );
+                },
+                child: Text('Mejor puntuación', style: const TextStyle(fontSize: 15), textAlign: TextAlign.center,),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
