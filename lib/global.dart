ac2c0978b0747767a1ffdf31bdca6fa1c62cf8e2
José Viloria <joseviloria@correo.unimet.edu.ve>
2026-02-02 16:44:19 -0400

    añadido funcionalidad guardado y carga de datos

--- lib/global.dart
+++ lib/global.dart
@@ -1,4 +1,6 @@
 import 'cardWidget.dart';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Global {
   static int clicks = 0;
@@ -8,4 +10,22 @@ class Global {
   static CardWidget? secondCardSelected;
   static CardWidgetState? firstCardState;
   static CardWidgetState? secondCardState;
+
+  static Future<void> saveData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('bestTime', playTime.inMilliseconds);
+    await prefs.setString('bestTimeString', durationString);
   }
+
+  static Future<int> loadBestTime() async {
+    final prefs = await SharedPreferences.getInstance();
+    int? bestTime = prefs.getInt('bestTime');
+    return bestTime ?? 0;
+  }
+
+  static Future<String> loadBestTimeString() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? bestTimeString = prefs.getString('bestTimeString');
+    return bestTimeString ?? 'No existe';
+  }
+}
